f3d2dd52ebd87a62b51e5ae3d81f71f3597a7413
Pablo H <pahefu@gmail.com>
2016-12-07 10:50:57 +0100

    Add files via upload

--- js/metadata.js
+++ js/metadata.js
@@ -4,6 +4,7 @@ var loadMetaData = function(){
 	regionHandler.addPseudoStar(0x164,"Nihonmatsuba ",0, "-1|17f|79|74","166538.1|350.5|336.7|309.6");
 	regionHandler.addPseudoStar(0x1f5,"De Caesarus",0, "-1|17f|79|74","166573.1|79.3|350.1|399.8");
 	regionHandler.addPseudoStar(0x95,"Polizeistaat",0, "-1|17f|79|74","166584.3|116.7|352.4|395.1");
+	regionHandler.addPseudoStar(0xA4,"Chevronech ",0, "-1|17f|79|74","166597.9|455.9|134.0|70.7");
 	
 	phoriaHandler.renderFrame();
 	
